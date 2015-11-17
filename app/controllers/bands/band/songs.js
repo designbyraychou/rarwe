a1708f29f4fa94bd1666fcb5b68fbb4a58659e58
Raymond Chou <designbyraychou@gmail.com>
2015-11-17 15:58:28 -0800

    complete chapter 12

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -2,47 +2,61 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  songCreationStarted: false,
-
-  searchTerm: '',
-  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function() {
-    var searchTerm = this.get('searchTerm').toLowerCase();
-    return this.get('model.songs').filter(function(song) {
-      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
-    });
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's'
+  },
+  
+  sortBy: 'ratingDesc',
+  sortProperties: Ember.computed('sortBy', function() {
+    var options = {
+      "ratingDesc": "rating:desc,title:asc",
+      "ratingAsc": "rating:asc,title:asc",
+      "titleDesc": "title:desc",
+      "titleAsc": "title:asc"
+    };
+    return options[this.get('sortBy')].split(',');
   }),
+
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+
+
+  songCreationStarted: false,
 
   canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
     return this.get('songCreationStarted') || this.get('model.songs.length');
   }),
 
-  sortBy: 'ratingDesc',
-  sortProperties: Ember.computed('sortBy', function() {
-    var options = {
-      "ratingDesc": "rating:desc,title:asc", "ratingAsc": "rating:asc,title:asc", "titleDesc": "title:desc",
-      "titleAsc": "title:asc",
-    };
-    return options[this.get('sortBy')].split(',');
+  searchTerm: '',
+
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function(song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !==
+        -1;
+    });
   }),
-  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
 
   actions: {
     enableSongCreation: function() {
       this.set('songCreationStarted', true);
     },
+
     updateRating: function(params) {
       var song = params.item,
-          rating = params.rating;
+        rating = params.rating;
 
       if (song.get('rating') === rating) {
         rating = 0;
       }
       song.set('rating', rating);
       song.save();
+
     },
+
     setSorting: function(option) {
       this.set('sortBy', option);
-    },
+    }
   }
 
 });
