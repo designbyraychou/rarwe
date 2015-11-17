ccd80c9901f85dc9bd9bc7c2f2e633d6fc3335d9
Raymond Chou <designbyraychou@gmail.com>
2015-11-17 12:59:38 -0800

    WIP query params

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -4,15 +4,32 @@ export default Ember.Controller.extend({
 
   songCreationStarted: false,
 
+  searchTerm: '',
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function(song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
+
   canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
     return this.get('songCreationStarted') || this.get('model.songs.length');
   }),
+
+  sortBy: 'ratingDesc',
+  sortProperties: Ember.computed('sortBy', function() {
+    var options = {
+      "ratingDesc": "rating:desc,title:asc", "ratingAsc": "rating:asc,title:asc", "titleDesc": "title:desc",
+      "titleAsc": "title:asc",
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
 
   actions: {
     enableSongCreation: function() {
       this.set('songCreationStarted', true);
     },
-
     updateRating: function(params) {
       var song = params.item,
           rating = params.rating;
@@ -22,8 +39,10 @@ export default Ember.Controller.extend({
       }
       song.set('rating', rating);
       song.save();
-
-    }
+    },
+    setSorting: function(option) {
+      this.set('sortBy', option);
+    },
   }
 
 });
