66faa8ff9e4c31459f70c3368a14024ebb1331cc
Raymond Chou <designbyraychou@gmail.com>
2015-11-18 12:33:00 -0800

    add helpers

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { capitalize } from '../../../helpers/capitalize';
 
 export default Ember.Controller.extend({
 
@@ -6,7 +7,7 @@ export default Ember.Controller.extend({
     sortBy: 'sort',
     searchTerm: 's'
   },
-  
+
   sortBy: 'ratingDesc',
   sortProperties: Ember.computed('sortBy', function() {
     var options = {
@@ -35,6 +36,11 @@ export default Ember.Controller.extend({
       return song.get('title').toLowerCase().indexOf(searchTerm) !==
         -1;
     });
+  }),
+
+  newSongPlaceholder: Ember.computed('model.name', function() {
+    var bandName = this.get('model.name');
+    return `New ${capitalize(bandName)} song`;
   }),
 
   actions: {
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
-import wait from '../../../utils/wait';
+import {
+  capitalize as capitalizeWords
+} from '../../../helpers/capitalize';
 
 export default Ember.Route.extend({
   model: function() {
@@ -9,7 +11,8 @@ export default Ember.Route.extend({
   actions: {
     didTransition: function() {
       var band = this.modelFor('bands.band');
-      document.title = `${band.get('name')} songs - Rock & Roll`;
+      var name = capitalizeWords(band.get('name'));
+      document.title = `${name} songs - Rock & Roll`;
     },
 
     createSong: function() {
